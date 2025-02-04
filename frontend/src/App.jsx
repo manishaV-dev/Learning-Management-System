ad73a648253454b77cc52c4e80aebf93467b551a
Manisha varma <manishavkapex1@gmail.com>
2025-02-04 22:21:03 +0530

    setup route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/student/Home";
+import CoursesList from "./pages/student/CoursesList";
+import CourseDetails from "./pages/student/CourseDetails";
+import MyEnrollment from "./pages/student/MyEnrollment";
+import Player from "./pages/student/Player";
+import Loading from "./components/student/Loading";
+import NotFound from "./pages/student/NotFound";
+
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold">Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/course-list" element={<CoursesList />} />
+        <Route path="/course-list/:input" element={<CoursesList />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollment />} />
+        <Route path="/player/:courseId" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+        {/* <Route path="/educator" element={<Educator />}>
+          <Route path="educator" element={<Dashboard />} />
+          <Route path="add-courses" element={<AddCourses />} />
+          <Route path="my-courses" element={<MyCourses />} />
+          <Route path="student-enrolled" element={<StudentEnrolled />} />
+        </Route> */}
+      </Routes>
     </>
   );
 }
