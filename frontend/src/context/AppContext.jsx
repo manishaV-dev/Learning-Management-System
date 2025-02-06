86d267cfb6c08cbf74f036a4c8d8fcfa230a36c8
Manisha varma <manishavkapex1@gmail.com>
2025-02-06 23:33:46 +0530

    course card section

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,9 +1,26 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
 
 export const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
-  const value = {};
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const [allCourses, setAllCourses] = useState([]);
+
+  // Fetch all courses
+  const fetchAllCourse = async () => {
+    setAllCourses(dummyCourses);
+  };
+
+  useEffect(() => {
+    fetchAllCourse();
+  }, []);
+
+  const value = {
+    currency,
+    allCourses,
+  };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
