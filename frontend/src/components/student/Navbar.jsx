5e6be0b245ee1b0e6cf0115a2e2ef912de30aa3b
Manisha varma <manishavkapex1@gmail.com>
2025-02-07 22:43:41 +0530

    cal avg rating and display it

--- frontend/src/components/student/Navbar.jsx
+++ frontend/src/components/student/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { AppContext } from "../../context/AppContext";
 
 const Navbar = () => {
   const isCourseListPage = location.pathname.includes("/course-list");
+  const {navigate} = useContext(AppContext)
 
   const { openSignIn } = useClerk();
   const { user } = useUser();
@@ -19,6 +21,7 @@ const Navbar = () => {
         src={assets.logo}
         alt="Logo"
         className="w-28 lg:w-32 cursor-pointer"
+        onClick={() => navigate("/")}
       />
 
       {/* for Large Device */}
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,16 +1,31 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
   const currency = import.meta.env.VITE_CURRENCY;
+  const navigate = useNavigate();
 
   const [allCourses, setAllCourses] = useState([]);
 
   // Fetch all courses
   const fetchAllCourse = async () => {
     setAllCourses(dummyCourses);
+  };
+
+  // function to calculate average rating of course
+  const calcRating = (course) => {
+    if (course.courseRatings.length === 0) {
+      // if no rating is given
+      return 0;
+    }
+    let totalRating = 0;
+    course.courseRatings.forEach((rating) => {
+      totalRating += rating.rating; // suppose if someone give 4 and other 5 then totalRating is 4 + 5 = 9
+    });
+    return totalRating / course.courseRatings.length; // 9/2- 4.5 (suppose only two people gave rating)
   };
 
   useEffect(() => {
@@ -20,6 +35,8 @@ const AppContextProvider = ({ children }) => {
   const value = {
     currency,
     allCourses,
+    navigate,
+    calcRating,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
