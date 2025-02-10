5d3028b5430a068e7d9e9aae776c542e3d850492
Manisha varma <manishavkapex1@gmail.com>
2025-02-10 18:30:52 +0530

    complete right side of course details page

--- frontend/src/pages/student/CourseDetails.jsx
+++ frontend/src/pages/student/CourseDetails.jsx
@@ -4,17 +4,20 @@ import { AppContext } from "../../context/AppContext";
 import Loading from "../../components/student/Loading";
 import { assets } from "../../assets/assets";
 import humanizeDuration from "humanize-duration";
+import Footer from "../../components/student/Footer";
 
 const CourseDetails = () => {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null);
   const [isOpen, setIsOpen] = useState({});
+  const [isEnrolled, setIsEnrolled] = useState(false);
   const {
     allCourses,
     calcRating,
     calculateChapterTime,
     calculateCourseDuration,
     calculateNoOfLectures,
+    currency,
   } = useContext(AppContext);
 
   const fetchCourseData = () => {
@@ -142,7 +145,9 @@ const CourseDetails = () => {
             </div>
           </div>
           <div className="py-20 text-sm md:text-base">
-            <h3 className="text-gray-800 text-xl font-semibold">Course Desciption</h3>
+            <h3 className="text-gray-800 text-xl font-semibold">
+              Course Desciption
+            </h3>
             <p
               dangerouslySetInnerHTML={{
                 __html: courseData.courseDescription,
@@ -151,9 +156,81 @@ const CourseDetails = () => {
             ></p>
           </div>
         </div>
+
         {/* Right side */}
-        <div></div>
+        <div className="z-10 max-w-[424px] shadow-2xl rounded-lg md:rounded-none overflow-hidden bg-white min-w-[200px] sm:min-w-[380px]">
+          <img src={courseData.courseThumbnail} alt={courseData.courseTitle} />
+          <div className="p-5">
+            <div className="flex items-center gap-2">
+              <img
+                className="w-3.5"
+                src={assets.time_left_clock_icon}
+                alt="time-left-clock-icon"
+              />
+              <p className="text-red-500">
+                <span className="font-bold">5 days</span> left at this price!{" "}
+              </p>
+            </div>
+            <div className="flex gap3 items-center pt-2">
+              <p className="text-gray-800 md:text-4xl text-2xl font-semibold">
+                {currency}
+                {(
+                  courseData.coursePrice -
+                  (courseData.discount * courseData.coursePrice) / 100
+                ).toFixed(2)}
+              </p>
+              <p className="ml-2 md:text-lg text-gray-500 line-through">
+                {currency}
+                {courseData.coursePrice}
+              </p>
+              <p className="ml-2 md:text-base text-gray-700">
+                {courseData.discount}% off
+              </p>
+            </div>
+
+            <div className="flex items-center text-sm md:text-base gap-4 pt-2 md:pt-4 text-gray-500">
+              <div className="flex items-center gap-1">
+                <img src={assets.star} alt="star" />
+                <p>{calcRating(courseData)}</p>
+              </div>
+
+              <div className="h-4 w-px bg-gray-500/40"></div>
+
+              <div className="flex items-center gap-1">
+                <img src={assets.time_clock_icon} alt="time-clock-icon" />
+                <p>{calculateCourseDuration(courseData)}</p>
+              </div>
+
+              <div className="h-4 w-px bg-gray-500/40"></div>
+
+              <div className="flex items-center gap-1">
+                <img src={assets.lesson_icon} alt="lesson-icon" />
+                <p>{calculateNoOfLectures(courseData)} lessons</p>
+              </div>
+            </div>
+
+            <button className="md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium cursor-pointer hover:bg-blue-700">
+              {isEnrolled ? "Already Enrolled" : "Enroll Now"}
+            </button>
+
+            <div className="pt-6">
+              <p className="md:text-xl text-lg font-medium text-gray-800">
+                What's in the course?
+              </p>
+
+              <ul className="ml-4 pt-2 text-sm md:text-base list-disc text-gray-500">
+                <li>Lifetime access with free updates.</li>
+                <li>Step-by-step, hands -on project guidance</li>
+                <li>Downloadable resources and source code</li>
+                <li>Quizzes to test your knowledge</li>
+                <li>Certificate of completion</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <Footer />
     </>
   ) : (
     <Loading />
